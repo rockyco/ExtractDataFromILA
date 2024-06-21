67bd259726dc1dcaf93f61b7cf756d6e053d535b
JayLei <jiejielei@gmail.com>
2024-06-21 23:12:16 +1000

    deleted:    iladata_evm_bad3.csv 	deleted:    iladata_evm_bad3.ila 	deleted:    iladata_evm_bad3.mat 	modified:   readILA.m 	new file:   waveform.csv 	new file:   waveform.mat 	new file:   waveform2CFO.mat

--- readILA.m
+++ readILA.m
@@ -1,27 +1,32 @@
+COL_NUM = 2; % Number of columns to assemble the complex data
+DAT_NUM = 1; % Number of data points in each column
 % Specify the file path and name
-opts = detectImportOptions("iladata_evm_bad3.csv"); 
+opts = detectImportOptions("waveform.csv"); 
 
 % Specify the columns and rows you want to read
-opts.SelectedVariableNames = ["rx_out0_TVALID", "rx_out0_TREADY", "rx_out0_TDATA"];
+opts.SelectedVariableNames = ["data2CFO_TVALID", "data2CFO_TDATA_im", "data2CFO_TDATA_re"];
 opts = setvartype(opts, 'string');
 
 % Read the CSV file
-data = readtable("iladata_evm_bad3.csv", opts);
+data = readtable("waveform.csv", opts);
 newData = zeros(2);
 compxData = complex(zeros(size(data,1), 1));
 j = 1;
-for i = 1:size(data, 1)
+for i = 1:size(data, 1) % Iterate through the rows of the data
     a = str2double(data{i,1}); % TVALID 
-    b = str2double(data{i,2}); % TREADY
+    % b = str2double(data{i,2}); % TREADY
+    b = 1;
     if (a == 1 && b == 1) % TVALID and TREADY are both high
-        tmp = data{i,3}; % TDATA
-        % Extract the 16-bit data
-        for k = 1:2
-            str = extractBetween(tmp, 4*(k-1)+1, 4*k); % Extract 4 hex characters
-            xdat = hex2dec(str); % Convert to decimal
-            isNegative = int16(bitget(xdat, 16)); % Check if the number is negative
-            convertedValue = int16(bitset(xdat,16,0)) + (-2^15)*isNegative; % Convert to signed 16-bit
-            newData(k) = convertedValue; % Store the data
+        for m = 1:COL_NUM % Iterate through the columns
+            tmp = data{i, m+1}; % TDATA
+            % Extract the 16-bit data
+            for k = 1:DAT_NUM % Iterate through the data points
+                str = extractBetween(tmp, 4*(k-1)+1, 4*k); % Extract 4 hex characters
+                xdat = hex2dec(str); % Convert to decimal
+                isNegative = int16(bitget(xdat, 16)); % Check if the number is negative
+                convertedValue = int16(bitset(xdat,16,0)) + (-2^15)*isNegative; % Convert to signed 16-bit
+                newData((m-1)*DAT_NUM + k) = convertedValue; % Store the data
+            end
         end
         compxData(j) = complex(newData(2), newData(1)); % Store the complex data
         j = j + 1; % Increment the index of the complex data array
@@ -34,4 +39,4 @@ compxData = compxData(1:j-1, :).'; % Remove the unused elements
 % Display the data
 disp(compxData);
 % Save the data to a .mat file
-save("iladata_evm_bad3.mat", "compxData"); 
+save("waveform2CFO.mat", "compxData"); 
